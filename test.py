062517975cf0f7eafdd897f861d42f0280ce6803
Anton Home <antonsolonovich@gmail.com>
2020-03-16 21:00:40 +0500

    Удалил import random

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-import re
-line = "1,2000"
-match = re.findall(r'\d+', line)
-#print(match[0] if match else 'Not found')
-print(int(match[0]))
-print(int(match[1]))
+import random
+from itertools import count
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+plt.style.use('fivethirtyeight')
+
+x_val = [1, 2, 4, 2]
+ch1_y_val = [3, 4, 2, 2]
+ch2_y_val = []
+plt.plot(x_val, ch1_y_val, label='Channel = 1')
+plt.show()
